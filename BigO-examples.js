aed4b82809dce07feea27c6167f40143b02df221
ashen-develops <ashenp.designs@gmail.com>
2020-12-01 11:57:42 -0600

    started assignment and made README

--- BigO-examples.js
+++ BigO-examples.js
@@ -78,7 +78,7 @@ console.log(howManyLessThanNitems([1,2,3], 2));
 console.log(howManyLessThanNitems([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100], 99 ));
 
 
-//---------------------LINEAR TIME O(n)-----------------------------------
+//---------------------LINEAR TIME O(n)  [[MEH]]------------------------------
 //Have running times that are directly proportional to the size (n) of the
 //input
 
@@ -98,3 +98,66 @@ function findMin(array){
     ticks: ticks
   };
 }
+console.log(findMin([1,2,3]));
+console.log(findMin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(findMin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]));
+
+
+//------------------POLYNOMIAL TIME O(n^k)  [[BIG OOF]]------------------------------
+//Has an exponential running time
+//Usualy consists of nested loops (an algorithim that requires two levels
+//of looping O(n^2), one with 3 would be O(n^3), etc...)
+
+//Working Example:
+
+function hasDuplicates(array) {
+  let ticks = 0, result = false;
+  for (let i = 0; i < array.length; i++){
+    ticks++;
+    for (let j = 0; j < array.length; j++) {
+      ticks++;
+      if (array[i] === array[j] && i !== j){
+        result = true;
+      }
+    }
+  }
+  return {
+    result: result,
+    ticks: ticks
+  };
+}
+console.log(hasDuplicates([1,2,3]));
+console.log(hasDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(hasDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]));
+
+
+//-------------------EXPONENTIAL TIME O(2^n) [[NO!]]-------------------------------
+//An input with a size of 2 will take 2^2 times or 4, an input with a
+//size of 10 will take 2^10 or 1024
+
+//Working Example:
+
+function countTriangle(layers) {
+  let ticks = 1;
+  let count = 0; // the number of dots we've counted so far
+  let layer = 0; // the current layer we're on
+  let lastLayer = 1; // the number of dots we counted in the previous layer
+  while (layer < layers) {
+    ticks++;
+    let newLayer = 0; // the number of dots we've counted so far in the current layer
+    for (let i = 0; i < lastLayer; i++){
+      ticks++;
+      newLayer += 2;
+    }
+    lastLayer = newLayer;
+    count += lastLayer;
+    layer++;
+  }
+  return {
+    result: count,
+    ticks: ticks
+  };
+}
+console.log(countTriangle(2));
+console.log(countTriangle(4));
+console.log(countTriangle(16));
